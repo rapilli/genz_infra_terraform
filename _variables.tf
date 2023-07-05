cea6a8b1530e8431629da95b14257fd78da7d161
silvijay <135017759+silvijay@users.noreply.github.com>
2023-07-05 17:59:44 +0530

    Update _variables.tf

--- _variables.tf
+++ _variables.tf
@@ -1,87 +1,31 @@
-variable "aws-region" {
-  description = "The AWS region"
-  type        = "string"
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
 }
 
-variable "aws-profile" {
-  description = "The name of the AWS shared credentials account."
-  type        = "string"
+variable "aws_secret_key" {
+  description = "AWS Secret Access Key"
+  type        = string
 }
 
-variable "instance-ami" {
-  description = "The AMI (Amazon Machine Image) that identifies the instance"
-  type        = "string"
-  default     = "ami-01419b804382064e4"
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-west-2"
 }
 
-variable "instance-type" {
-  description = "The instance type to be used"
-  type        = "string"
+variable "ec2_ami" {
+  description = "EC2 AMI ID"
+  type        = string
+}
+
+variable "ec2_instance_type" {
+  description = "EC2 Instance Type"
+  type        = string
   default     = "t2.micro"
 }
 
-variable "instance-key-name" {
-  description = "The name of the SSH key to associate to the instance. Note that the key must exist already."
-  type        = "string"
-  default     = ""
-}
-
-variable "iam-role-name" {
-  description = "The IAM role to assign to the instance"
-  type        = "string"
-  default     = ""
-}
-
-variable "instance-associate-public-ip" {
-  description = "Defines if the EC2 instance has a public IP address."
-  type        = "string"
-  default     = "true"
-}
-
-variable "user-data-script" {
-  description = "The filepath to the user-data script, that is executed upon spinning up the instance"
-  type        = "string"
-  default     = ""
-}
-
-variable "instance-tag-name" {
-  description = "instance-tag-name"
-  type        = "string"
-  default     = "EC2-instance-created-with-terraform"
-}
-
-variable "vpc-cidr-block" {
-  description = "The CIDR block to associate to the VPC"
-  type        = "string"
-  default     = "10.0.0.0/16"
-}
-
-variable "subnet-cidr-block" {
-  description = "The CIDR block to associate to the subnet"
-  type        = "string"
-  default     = "10.0.1.0/24"
-}
-
-variable "vpc-tag-name" {
-  description = "The Name to apply to the VPC"
-  type        = "string"
-  default     = "VPC-created-with-terraform"
-}
-
-variable "ig-tag-name" {
-  description = "The name to apply to the Internet gateway tag"
-  type        = "string"
-  default     = "aws-ig-created-with-terraform"
-}
-
-variable "subnet-tag-name" {
-  description = "The Name to apply to the VPN"
-  type        = "string"
-  default     = "VPN-created-with-terraform"
-}
-
-variable "sg-tag-name" {
-  description = "The Name to apply to the security group"
-  type        = "string"
-  default     = "SG-created-with-terraform"
+variable "ec2_key_pair" {
+  description = "EC2 Key Pair Name"
+  type        = string
 }
