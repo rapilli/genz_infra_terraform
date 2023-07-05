6cb1364a4052558d8e9c0e8ae1150286848fe15a
silvijay <135017759+silvijay@users.noreply.github.com>
2023-07-05 18:19:04 +0530

    Update _main.tf

--- _main.tf
+++ _main.tf
@@ -1,15 +1,12 @@
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.aws_region
+  region = "us-east-2"
 }
 
-resource "aws_instance" "example_instance" {
-  ami           = var.ec2_ami
-  instance_type = var.ec2_instance_type
-  key_name      = var.ec2_key_pair
+resource "aws_instance" "example" {
+  ami           = "ami-0c94855ba95c71c99"
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "Example Instance"
+    Name = "ExampleInstance"
   }
 }
