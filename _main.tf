0a151408ff3be8c68647ce8d0b2299ab57a8aa48
silvijay <135017759+silvijay@users.noreply.github.com>
2023-07-07 17:18:19 +0530

    Update _main.tf

--- _main.tf
+++ _main.tf
@@ -1,12 +1,15 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-2"  # Replace with your desired AWS region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c94855ba95c71c99"
-  instance_type = "t2.micro"
+  ami           = var.ami
+  instance_type = var.instance_type
 
   tags = {
-    Name = "ExampleInstance"
+    Name = "MG_silvijay"
   }
 }
+
