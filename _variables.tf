235271f8402e0dbaa43ff2fb9e07081418c84655
silvijay <135017759+silvijay@users.noreply.github.com>
2023-08-03 17:24:31 +0530

    Update _variables.tf

--- _variables.tf
+++ _variables.tf
@@ -1,23 +1,68 @@
-variable "aws_access_key" {
-  description = "AWS Access Key"
-  type        = string
-  default     = ""
+variable "aws_region" {
+  description = "AWS region where RDS will be created."
+  default     = "us-east-1"
 }
 
-variable "aws_secret_key" {
-  description = "AWS Secret Access Key"
-  type        = string
-  default     = ""
+variable "db_identifier" {
+  description = "The name of the RDS instance."
+  default     = "example-db"
 }
 
-variable "ami" {
-  description = "Amazon Machine Image ID"
-  type        = string
-  default     = "ami-0103f211a154d64a6"
+variable "db_allocated_storage" {
+  description = "The amount of storage to allocate in GB."
+  default     = 20
 }
 
-variable "instance_type" {
-  description = "EC2 Instance Type"
-  type        = string
-  default     = "t2.micro"
+variable "db_engine" {
+  description = "The name of the database engine to be used."
+  default     = "mysql"
+}
+
+variable "db_engine_version" {
+  description = "The version of the database engine."
+  default     = "8.0.26"
+}
+
+variable "db_instance_class" {
+  description = "The compute and memory capacity of the RDS instance."
+  default     = "db.t2.micro"
+}
+
+variable "db_username" {
+  description = "The master username for the RDS instance."
+  default     = "admin"
+}
+
+variable "db_password" {
+  description = "The password for the master username."
+}
+
+variable "db_name" {
+  description = "The name of the default database to be created."
+  default     = "mydb"
+}
+
+variable "db_port" {
+  description = "The port on which the database accepts connections."
+  default     = 3306
+}
+
+variable "db_parameter_group_name" {
+  description = "The name of the parameter group to associate with the RDS instance."
+  default     = "default.mysql8.0"
+}
+
+variable "db_publicly_accessible" {
+  description = "Whether the RDS instance should have a public IP address."
+  default     = false
+}
+
+variable "db_skip_final_snapshot" {
+  description = "Whether to skip the final snapshot when the RDS instance is deleted."
+  default     = true
+}
+
+variable "db_security_group_id" {
+  description = "The ID of the security group to associate with the RDS instance."
+  default     = aws_security_group.db_sg.id
 }
